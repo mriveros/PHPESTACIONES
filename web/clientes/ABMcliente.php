b16345daf850d5d2620d9ef4632249ad9e89d75f
mriveros <mriveros.py@gmail.com>
2015-06-23 15:36:27 -0400

    conmitemaos con nustro nuevo branch mriveros

--- web/clientes/ABMcliente.php
+++ web/clientes/ABMcliente.php
@@ -15,7 +15,7 @@ $catego=  $_SESSION["categoria_usuario"];
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>ONM- Clientes</title>
+    <title>ONM-Clientes</title>
     <!-- Bootstrap Core CSS -->
     <link href="../../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- MetisMenu CSS -->
